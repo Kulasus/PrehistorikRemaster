10ba8213ab80eabf1879233181fa601001b0bc50
Lukáš Kondiolka <lukaskondiolka@seznam.cz>
2020-01-01 17:32:14 +0100

    Added basic game logic

---
+++ PlatformerGame/src/platformergame/LevelData.java
@@ -0,0 +1,23 @@
+package platformergame;
+
+public class LevelData {
+    /*  DESIGN INFO: 
+        resolutin = 28 columns x 7 layers
+        0 = Nothing
+        1 = Platform
+        2 = Enemy TODO
+        3 = Spawn Point TODO
+        4 = Collectible TODO
+        5 = Level end TODO  
+    */
+    public static String[] LEVEL1 = new String[]{
+        "0000000000000000000000000000",
+        "0000000000000000000000000000",
+        "1111111110000000111100000000",
+        "0000000000000000000000000000",
+        "0000000000111100000000000000",
+        "0000000000000000000000000000",
+        "1111111111111111111000001111"
+    };
+    
+}
--- PlatformerGame/src/platformergame/PlatformerGame.java
+++ PlatformerGame/src/platformergame/PlatformerGame.java
@@ -5,12 +5,18 @@
  */
 package platformergame;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -18,34 +24,154 @@ import javafx.stage.Stage;
  * @author lukas
  */
 public class PlatformerGame extends Application {
+   
+    private HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
+    
+    private ArrayList<Node> platforms = new ArrayList<Node>();
+    
+    private Pane appRoot = new Pane();
+    private Pane gameRoot = new Pane();
+    private Pane uiRoot = new Pane();
+    
+    private Node player;
+    private Point2D playerVelocity = new Point2D(0,0);
+    private boolean canJump = true;
+    
+    private int levelWidth;
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws Exception{
+        initContent();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(appRoot);
+        scene.setOnKeyPressed(event -> keys.put(event.getCode(),true));
+        scene.setOnKeyReleased(event -> keys.put(event.getCode(), false));
+        primaryStage.setTitle("Prehistorik");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        AnimationTimer timer = new AnimationTimer(){
+            @Override
+            public void handle(long now){
+                update();
+            }
+        };
+        timer.start();
+        
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    private void jumpPlayer(){
+        if(canJump){
+            playerVelocity = playerVelocity.add(0,-30);
+            canJump = false;
+        }
+    }
+    
+    private void movePlayerX(int value){
+        boolean movingRight = value > 0;
+        
+        for (int i = 0; i < Math.abs(value); i++) {
+            for (Node platform : platforms) {
+                if(player.getBoundsInParent().intersects(platform.getBoundsInParent())){
+                    if(movingRight){
+                        if(player.getTranslateX() + 40 == platform.getTranslateX()){
+                            return;
+                        }
+                    }
+                    else{
+                        if(player.getTranslateX() == platform.getTranslateX() +60){
+                            return;
+                        }
+                    }
+                }
+            }
+            player.setTranslateX(player.getTranslateX() + (movingRight ? 1 : -1));
+        }
+    }
+    
+    private void movePlayerY(int value){
+        boolean movingDown = value > 0;
+
+            for (int i = 0; i < Math.abs(value); i++) {
+                for (Node platform : platforms) {
+                    if(player.getBoundsInParent().intersects(platform.getBoundsInParent())){
+                        if(movingDown){
+                            if(player.getTranslateY() + 40 == platform.getTranslateY()){
+                                player.setTranslateY(player.getTranslateY()-1);
+                                canJump = true;
+                                return;
+                            }
+                        }
+                        else{
+                            if(player.getTranslateY() == platform.getTranslateY() +60){
+                                return;
+                            }
+                        }
+                    }
+                }
+                player.setTranslateX(player.getTranslateY() + (movingDown ? 1 : -1));
+            }
+    }
+    
+    private void update(){
+        if(isPressed(KeyCode.W) && player.getTranslateY() >= 5){
+            jumpPlayer();
+        }
+        if(isPressed(KeyCode.A) && player.getTranslateX() >= 5){
+            movePlayerX(-5);
+        }
+        if(isPressed(KeyCode.D) && player.getTranslateX() * 40 <= levelWidth - 5){
+            movePlayerX(5);
+        }
+        if(playerVelocity.getY() < 10){
+            playerVelocity = playerVelocity.add(0,1);
+        }
+        movePlayerY((int)playerVelocity.getY());
+    }
+    
+    private void initContent() {
+        Rectangle background = new Rectangle(1200,720);
+        levelWidth = LevelData.LEVEL1[0].length() * 60; //60 is blocksize
+        
+        for (int i = 0; i < LevelData.LEVEL1.length; i++) {
+            String line = LevelData.LEVEL1[i];
+            for (int j = 0; j < line.length(); j++) {
+                switch(line.charAt(j)){
+                    case '0':
+                        break;
+                    case '1':
+                        Node platform = createEntity(j*60, i*60, 60, Color.BROWN);
+                        platforms.add(platform);
+                        break;
+                }
+            }  
+        }
+        player = createEntity(0,600,40,40,Color.BLUE)
+        player.translateXProperty().addListener((obs, old, newValue) -> {
+            int offset = newValue.intValue();
+            if(offset > 640 && offset < levelWidth - 640){
+                gameRoot.setLayoutX(-(offset - 640));
+            }
+        });
+        appRoot.getChildren().addAll(background,gameRoot,uiRoot);
+    }
+
+    private Node createEntity(int x, int y, int w, int h, Color color){
+        Rectangle entity = new Rectangle(w,h);
+        entity.setTranslateX(x);
+        entity.setTranslateY(y);
+        entity.setFill(color);
+        
+        gameRoot.getChildren().add(entity);
+        return entity;        
+    }
+    
+    private boolean isPressed(KeyCode keyCode) {
+        return keys.getOrDefault(keyCode, false);
     }
     
 }
